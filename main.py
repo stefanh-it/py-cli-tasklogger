6f0abce897b79e832a742e8ed20550f0619550e4
Stefan <chinaxsteve@gmail.com>
2024-01-26 21:42:48 +0100

    setup of readme

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import time
+import datetime
 import typer
+
+class Task():
+    duration = 0
+    start_time = 0
+    def __init__(self, title):
+        self.title = title
+        
+        
 
 def main(name: str):
     print(f"Hi {name}")
