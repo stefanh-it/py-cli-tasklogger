6e01b78bde9576e96ddd2e68c949bd38ebbe49d1
Dmtr <github@dmtrh.eu>
2024-01-26 22:24:33 +0100

    merge commits

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import datetime
 import time
 import typer
 
+# from datetime import timedelta
+# delta = timedelta(
+#     days=50,
+#     seconds=27,
+#     microseconds=10,
+#     milliseconds=29000,
+#     minutes=5,
+#     hours=8,
+#     weeks=2
+# )
+
 class Task():
     duration = 0
     start_time = 0
@@ -10,15 +21,16 @@ class Task():
         self.title = title
 
     def start_task(self):
+        print(f'Start {self.title} at:')
         self.start_time = datetime.datetime.now()
         print(self.start_time)
 
     def end_task(self):
+        print(f'Stop {self.title} at:')
         self.end_time = datetime.datetime.now()
         print(self.end_time)
         if self.end_time != self.start_time:
             duration = self.end_time - self.start_time
-            print(duration)
 
 def main():
     x = Task('Erste Aufgabe')
